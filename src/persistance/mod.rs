00f5174166ea44e892c3c43a0401401e8ec8dd1d
Alejandro Gaviria <mr.gaviria@gmail.com>
2017-09-18 10:08:26 -0400

    refactored postgres connection to handle rocket's request guard esque , failing with  if no pool is managed or failing with  if no connections are available.

--- src/persistance/mod.rs
+++ src/persistance/mod.rs
@@ -1,26 +1,54 @@
 pub mod error;
 
-use r2d2::{Config, ManageConnection, Pool, PooledConnection};
-use r2d2_postgres::PostgresConnectionManager;
-use self::error::Error;
+use std::ops::Deref;
+use std::env;
 
-pub struct Persistance<T: ManageConnection> {
-    pool: Pool<T>,
+use postgres::Connection as PostgresConnection;
+use r2d2::{Config, Pool, PooledConnection};
+use r2d2_postgres::{TlsMode, PostgresConnectionManager};
+use rocket::http::Status;
+use rocket::request::{self, FromRequest};
+use rocket::{State, Request, Outcome};
+
+pub struct PgConn(pub PooledConnection<PostgresConnectionManager>);
+
+// PgConn rocket request guard attempts to retrieve a single connection from the
+// managed pool.  If no pool is currently managed, fails with an
+// `InternalServerError` status.  If no connections are available, fails with a
+// `ServiceUnavailable` status.
+impl<'a, 'r> FromRequest<'a, 'r> for PgConn {
+    type Error = ();
+
+    fn from_request(req: &'a Request<'r>) -> request::Outcome<PgConn, ()> {
+        let pool = req.guard::<State<conn>>()?;
+        match pool.get() {
+            Ok(conn) => Outcome::Success(PgConn(conn)),
+            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ()))
+        }
+    }
 }
 
-impl<T: ManageConnection> Persistance<T> {
-    pub fn new(manager: T) -> Result<Persistance<T>, Error> {
-        let config = Config::default();
-        let pool = Pool::new(config, manager)?;
+impl Deref for PgConn {
+    type Target = PostgresConnection;
 
-        Ok(Persistance { pool: pool })
-    }
-
-    pub fn get_conn(&self) -> Result<PooledConnection<T>, Error> {
-        let conn = self.pool.get()?;
-
-        Ok(conn)
+    fn deref(&self) -> &Self::Targe {
+        &self.0
     }
 }
 
-pub type PgDatabase = Persistance<PostgresConnectionManager>;
+type PgDb = Pool<PostgresConnectionManager>;
+
+pub fn pg_init_pool_mgr() -> PgDb {
+    // DATABASE_URL is a required nomenclature for diesel.
+    let db_path = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    // TODO:
+    // ------------------------------------------------------------------------
+    // * Implement TLS.
+    //     - use security framework or
+    //     - stick with plain vanilla openssl x509 filetype ?
+    //
+    // * Setup pg configs from settings.toml
+    let config = Config::default();
+    let manager = PostgresConnectionManager::new(db_path, TlsMode::None).unwrap();
+    Pool::new(config, manager).expect("PG DB POOL")
+}
