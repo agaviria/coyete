246976406bb087be34b272c4ac15410d9743b0fb
Alejandro Gaviria <mr.gaviria@gmail.com>
2017-09-23 01:53:25 -0400

    extracted persistance data layer to external coyete-data library.

---
+++ coyete-data/src/lib.rs
@@ -0,0 +1,16 @@
+#![feature(plugin)]
+#![feature(custom_derive)]
+#![feature(custom_attribute)]
+
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_codegen;
+extern crate chrono;
+extern crate r2d2;
+extern crate r2d2_diesel;
+extern crate r2d2_postgres;
+extern crate dotenv;
+
+pub mod persistance;
+pub mod models;
