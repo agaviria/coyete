753cac15ba8fc1d48aa4afd0d2c66dfea502bcdd
Alejandro Gaviria <mr.gaviria@gmail.com>
2017-09-24 15:27:31 -0400

    Changed hash_data to hash_password with salt as function params. This edit will allow us to decode signature for hash verification.

--- src/models/crypto.rs
+++ src/models/crypto.rs
@@ -2,6 +2,7 @@ use rocket::Outcome;
 use rocket::request::{self, Request, FromRequest};
 use rocket::http::Status;
 use argon2::{self, Config, ThreadMode, Variant, Version};
+use api::response::{self, APIResponse};
 
 // crypto constants
 const SALT_SIZE: usize = 32;
@@ -9,7 +10,7 @@ const HASH_SIZE: usize = 32;
 const MIN_PWD_SIZE: usize = 6;
 // authentication guard constants
 const AUTH_PREFIX: &'static str = "x-auth ";
-const AUTH_SEPERATOR: &'static str = ":";
+const AUTH_SEPERATOR: &'static str = ".";
 
 
 #[derive(Serialize)]
@@ -40,75 +41,38 @@ impl<'a 'r> FromRequest<'a, 'r> for AuthorizationBearer {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Cipher {
-    pub salt: Vec<u8>,
-    pub hash: Vec<u8>,
+pub fn hash_password<'a>(password: &'a str, salt: &'a str) -> String {
+    // argon_config is an Argon2id hybrid version. It follows the Argon2i approach
+    // for the first pass over memory and the Argon2d approach for subsequent passes.
+    let argon_config = Config {
+        variant: Variant::Argon2id,
+        version: Version::Version13,
+        mem_cost: 256,
+        time_cost: 3,
+        lanes: 4,
+        thread_mode: ThreadMode::Parallel,
+        secret: &[],
+        ad: &[],
+        hash_length: 32,
+    };
+
+    argon2::hash_encoded(&password, &salt, &argon_config)
+        .iter()
+        .map(|b| format!("{:02X}", b))
+        .collect()
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct PassThruValidator {
-    value: String,
-}
-// PassThruValidator under the hood implements a helper which places a minimum
-// string parameter of $MIN_PWD_SIZE. In adherence to best security practices.
-impl PassThruValidator {
-    pub fn new(value: &str) -> Result<PassThruValidator, Error> {
-        if value.len() < MIN_PWD_SIZE {
-            return Err(Error::MinLen(MIN_PWD_SIZE));
-        }
-        Ok(PassThruValidator { value: value.to_string() })
-    }
+// generate_salt creates salt vector of 32 bytes
+pub fn generate_salt() -> Vec<u8> {
+    nonce(32).as_bytes().to_vec();
 }
 
-pub struct HashEncoder;
+// nonce takes a usize parameter for length
+fn nonce(take: usize) -> String {
+    use rand::{self, Rng};
 
-impl HashEncoder {
-    pub fn generate(sub: &str, credential: &PassThruValidator) -> Result<Cipher, Error> {
-        // Argon2id is a hybrid version. It follows the Argon2i approach for the
-        // first pass over memory and the Argon2d approach for subsequent passes.
-        //
-        // TODO:
-        // * Add argon2id_config Config to settings.toml
-        // * Include argon2::verify_encode() to integration tests
-        let argon_config = Config {
-            variant: Variant::Argon2id,
-            version: Version::Version13,
-            mem_cost: 256,
-            time_cost: 3,
-            lanes: 4,
-            thread_mode: ThreadMode::Parallel,
-            secret: &[],
-            ad: &[],
-            hash_length: 32,
-        };
-
-        let salt = generate_salt();
-        let hash = argon2::hash_encoded(&credential.value, &salt, &argon_config);
-        Ok(Cipher {
-            salt: salt,
-            hash: hash.to_vec(),
-        })
-    }
-
-    pub fn compare(credential: &PassThruValidator, salt: String, conf: &Config) -> Result<Cipher, Error> {
-        let inspect  = argon2::hash_encoded(&credential, &salt, conf)
-            .map_err(|_| Error::Mismatch())?;
-
-        Ok(inspect)
-    }
-
-    // generate_salt creates salt vector of 32 bytes
-    pub fn generate_salt() -> Vec<u8> {
-        nonce(32).as_bytes().to_vec();
-    }
-
-    // nonce takes a usize parameter for length
-    fn nonce(take: usize) -> String {
-        use rand::{self, Rng};
-
-        rand::thread_rng()
-            .gen_ascii_chars()
-            .take(take)
-            .collect::<String>()
-    }
+    rand::thread_rng()
+        .gen_ascii_chars()
+        .take(take)
+        .collect::<String>()
+}
